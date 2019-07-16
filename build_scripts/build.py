8c2bb20790c1fb098cbeb2a2a5b4326675d2deae
Yves Bastide <yves@botify.com>
2019-07-16 10:45:16 +0200

    Fix relative imports

--- build_scripts/build.py
+++ build_scripts/build.py
@@ -9,24 +9,29 @@ from writers import write_services
 
 
 def load(path: str, session_: Session) -> Config:
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         file_contents = yaml.safe_load(f)
-    if file_contents.get('services') == 'all':
-        file_contents['services'] = session_.get_available_services()
-    if not all((service in session_.get_available_services() for service in file_contents.get('services'))):
-        raise ValueError('Invalid service name.')
+    if file_contents.get("services") == "all":
+        file_contents["services"] = session_.get_available_services()
+    if not all(
+        (
+            service in session_.get_available_services()
+            for service in file_contents.get("services")
+        )
+    ):
+        raise ValueError("Invalid service name.")
     return Config(
-        file_contents.get('services', session_.get_available_services()),
-        file_contents.get('with_docs', False),
-        file_contents.get('with_clients', True),
-        file_contents.get('with_service_resources', True),
-        file_contents.get('with_paginators', True),
-        file_contents.get('with_waiters', True),
-        file_contents.get('package_name', 'boto3_type_annotations'),
-        file_contents.get('module_name', 'boto3_type_annotations'),
-        file_contents.get('version'),
-        file_contents.get('readme', '../README.md'),
-        file_contents.get('license', '../LICENSE')
+        file_contents.get("services", session_.get_available_services()),
+        file_contents.get("with_docs", False),
+        file_contents.get("with_clients", True),
+        file_contents.get("with_service_resources", True),
+        file_contents.get("with_paginators", True),
+        file_contents.get("with_waiters", True),
+        file_contents.get("package_name", "boto3_type_annotations"),
+        file_contents.get("module_name", "boto3_type_annotations"),
+        file_contents.get("version"),
+        file_contents.get("readme", "../README.md"),
+        file_contents.get("license", "../LICENSE"),
     )
 
 
@@ -34,9 +39,9 @@ def build(session: Session, config: Config):
     write_services(session, config)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(dirname(abspath(__file__)))
     if len(argv) < 2:
-        raise ValueError('Missing config path.')
+        raise ValueError("Missing config path.")
     session = Session()
     build(session, load(argv[1], session))
